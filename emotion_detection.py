444b53fdade292193befa5ae4e94651b8ff55d77
qilinbake <thevictorvictorm@gmail.com>
2024-11-10 17:32:17 -0500

    Changed function

--- emotion_detection.py
+++ emotion_detection.py
@@ -5,4 +5,7 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     json_obj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = json_obj, headers = headers)
-    return response.text
+    emotions = response.text['emotionPredictions'][0]
+    return emotions
+
+print(emotion_detector("I love this new technology"))
