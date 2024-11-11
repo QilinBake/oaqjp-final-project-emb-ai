49c69a0b3878fbfc64ff6787172caa627e61ec02
qilinbake <thevictorvictorm@gmail.com>
2024-11-10 19:20:36 -0500

    Made server.py comply with PEP8 via pylint.

--- server.py
+++ server.py
@@ -1,9 +1,15 @@
+''' 
+    Flask application that given a text from the website, 
+    displays the associated emotions with that text as well as the dominant emotion.
+'''
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotion_detect():
+    ''' Retrieves HTTP request and returns formatted response '''
     response = emotion_detector(request.args.get('textToAnalyze'))
     if not response['dominant_emotion']:
         return "Invalid text! Please try again!"
@@ -14,6 +20,7 @@ def emotion_detect():
 
 @app.route("/")
 def render_index():
+    ''' Display index home page '''
     return render_template("index.html")
 
 if __name__ == '__main__':
